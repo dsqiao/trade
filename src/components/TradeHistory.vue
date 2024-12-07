9158e107047e8da301e3ad970e83b676c6dba132
dsqiao <2420392704@qq.com>
2024-12-07 21:59:15 +0800

    update

--- src/components/TradeHistory.vue
+++ src/components/TradeHistory.vue
@@ -1,20 +1,28 @@
 <template>
 <div>Trade History</div>
-<div>2024.12.6 转出 1000 sui 到钱包做 T 5e4R8XLG9noGZ28ez1LpaYcSZ2yM7YTRrfGzrcxhx8ZD </div>
+<div>2024.12.6 转出 1000 sui 到钱包做 T <span @click="jump('5e4R8XLG9noGZ28ez1LpaYcSZ2yM7YTRrfGzrcxhx8ZD')" class="digest">5e4R8XLG9noGZ28ez1LpaYcSZ2yM7YTRrfGzrcxhx8ZD</span> </div>
 <div>{{ `sui 收益: ${suiGain.toFixed(9)}` }}</div>
 <div>{{ `usdc 收益: ${usdcGain.toFixed(6)}` }}</div>
+<div v-if="suiGain < 0">{{ `卖出 SUI 均价 ${ - usdcGain / suiGain}` }}</div>
+<div v-for="(tran, index) of suiTradeData" :key="index" class="transaction" :class="tran.direction === direction.SELL ? 'sell' : 'buy'">
+  <span @click="jump(tran.digest)" class="digest">digest</span>
+  <span class="direction">{{ tran.direction === direction.SELL ? '卖出' : '买入' }}</span>
+  <span class="detail">{{ tran.direction === direction.SELL ? `${tran.sui} SUI => ${tran.usdc} USDC` : `${tran.usdc} USDC => ${tran.sui} SUI`}}</span>
+  <span class="price">{{ `均价 ${(tran.usdc / tran.sui).toFixed(4)}` }}</span>
+  <span class="t">{{ tran.t || 'null' }}</span>
+</div>
 </template>
 <script setup>
 import { ref } from 'vue';
 import { suiTradeData, direction } from '../data/sui.js'
-// import { useRoute } from 'vue-router';
-// const route = useRoute();
-// const coin = route.params.stock;
 
+const jump = (digest) => {
+  window.open(`https://suivision.xyz/txblock/${digest}?tab=Changes`)
+}
 const suiGain = ref(0)
 const usdcGain = ref(0)
 for (const trans of suiTradeData) {
-  if (trans.direction === direction.SuiToUsdc) { // 卖 sui，买 usdc
+  if (trans.direction === direction.SELL) { // 卖 sui，买 usdc
     suiGain.value -= trans.sui;
     usdcGain.value += trans.usdc;
   } else { // 卖 usdc，买 sui
@@ -23,3 +31,34 @@ for (const trans of suiTradeData) {
   }
 }
 </script>
+<style>
+.transaction>span {
+  display: inline-block;
+  border: 1px black solid;
+  height: 2rem;
+  line-height: 2rem;
+  padding-left: 1rem;
+}
+.sell {
+  background-color: rgb(85, 23, 23);
+}
+.buy {
+  background-color: rgb(33, 83, 33);
+}
+.digest {
+  cursor: pointer;
+  width: 5rem;
+}
+.direction {
+  width: 4rem;
+}
+.detail {
+  width: 30rem;
+}
+.price {
+  width: 10rem;
+}
+.t {
+  width: 10rem;
+}
+</style>
--- src/data/sui.js
+++ src/data/sui.js
@@ -1,18 +1,23 @@
-const SuiToUsdc = 0;
-const UsdcToSui = 1;
+const SELL = 0;
+const BUY = 1;
 const direction = {
-  SuiToUsdc: 0,
-  UsdcToSui: 1,
+  SELL: 0,
+  BUY: 1,
 }
-const SELL = SuiToUsdc
 const suiTradeData = [
-  { digest: 'AKjgSJtp3m6EmWv4QMyn7RqV1vqdntRBNZ9JahVNcASW', direction: SuiToUsdc, sui: 1000.006227092, usdc: 4346 },
-  { digest: 'EFvBDYsRq5xuHbBCib126Tsqn3Dsosd4EQ8Mykw3aBgJ', direction: UsdcToSui, sui: 1007.311626443, usdc: 4300 },
-  { digest: 'DjLNFJ3geif2oQTfTpeAJShqu3T5urwjLkdXiXP68ugv', direction: SuiToUsdc, sui: 1000.004184896, usdc: 4275.0276 },
-  { digest: 'Fd18VVTChDFSw6ysv2RfT4bsvWYZk7f6azBest97CVh2', direction: UsdcToSui, sui: 1005.400903906, usdc: 4269.735 },
-  { digest: '4HBt2CEy3wCNSmZnjh8T1t1LADqaAEMvzRrSXGWKJUcV', direction: UsdcToSui, sui: 11.983510807, usdc: 50 },
-  { digest: '8PRNsvqP9LpeFaZdEZxkX9pku2yYzrqJq1u4SEe3sME7', direction: SuiToUsdc, sui: 11.983422768, usdc: 50.758854 },
-  { digest: 'BbeonZNQ7WnbHFU4z8z6JCFb1iND2uvUmgk3wdvLkvPD', direction: SELL, sui: 300.011030724, usdc: 1297.14390 },
+  { digest: 'AKjgSJtp3m6EmWv4QMyn7RqV1vqdntRBNZ9JahVNcASW', direction: SELL, sui: 1000.006227092, usdc: 4346, t: '1' },
+  { digest: 'EFvBDYsRq5xuHbBCib126Tsqn3Dsosd4EQ8Mykw3aBgJ', direction: BUY, sui: 1007.311626443, usdc: 4300, t: '1' },
+  { digest: 'DjLNFJ3geif2oQTfTpeAJShqu3T5urwjLkdXiXP68ugv', direction: SELL, sui: 1000.004184896, usdc: 4275.0276, t: '2' },
+  { digest: 'Fd18VVTChDFSw6ysv2RfT4bsvWYZk7f6azBest97CVh2', direction: BUY, sui: 1005.400903906, usdc: 4269.735, t: '2' },
+  { digest: '4HBt2CEy3wCNSmZnjh8T1t1LADqaAEMvzRrSXGWKJUcV', direction: BUY, sui: 11.983510807, usdc: 50, t: '3' },
+  { digest: '8PRNsvqP9LpeFaZdEZxkX9pku2yYzrqJq1u4SEe3sME7', direction: SELL, sui: 11.983422768, usdc: 50.758854, t: '3' },
+  { digest: 'BbeonZNQ7WnbHFU4z8z6JCFb1iND2uvUmgk3wdvLkvPD', direction: SELL, sui: 300.011030724, usdc: 1297.143905 },
+  { digest: 'Czpk3DHnZFeNn8XawXuQmFTJ4isuCEKfCmRuFdC3jBP8', direction: SELL, sui: 300.006073364, usdc: 1310.140771, t: '4' },
+  { digest: '5q4z2pxCX6hUqxq2NgwHXxc6YDnge1WLHfSNoP3Gtm4Y', direction: BUY, sui: 300.941942388, usdc: 1300, t: '4' },
+  { digest: 'GGAqmBsW3v81FM63xwbVUPm2xMsw3DkJU4cEvXDmmZpN', direction: SELL, sui: 300.015812732, usdc: 1306.891448, t: '6' },
+  { digest: '2zDfZ3aNk8oTcmpoUfSK6x6UMb3V2ZhtQL6gs74VbJaw', direction: SELL, sui: 300.005240752, usdc: 1329.63248, t: '5' },
+  { digest: '4td7oaamTtb6AtTumHdkqnn3C4E5itZY8Th9XGY9MdNY', direction: BUY, sui: 299.586900332, usdc: 1320, t: '5' },
+  { digest: '4y7cqSRU1R5agz2ds7EroArpw8odpEJrAj4EYsA9wkRa', direction: BUY, sui: 300.064195511, usdc: 1299.613, t: '6'}
 ]
 
 export {
