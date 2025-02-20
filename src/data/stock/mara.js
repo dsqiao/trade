5c6f9a3b118e045e8a4cff67fa0a5b208899e305
dsqiao <2420392704@qq.com>
2025-02-20 10:05:36 +0800

    update

--- src/data/stock/mara.js
+++ src/data/stock/mara.js
@@ -347,7 +347,8 @@ const data = [
     trans: [
       { day: 7, price: 17, number: 150, direction: BUY, fee: 1.45 },
       { day: 13, price: 15.92, number: 50, direction: BUY, fee: 1.15, t: 'grid18' },
-      { day: 14, price: 17, number: 50, direction: SELL, fee: 0, t: 'grid18 收益 54' },
+      { day: 14, price: 17, number: 50, direction: SELL, fee: 1.18, t: 'grid18 收益 54' },
+      { day: 18, price: 16, number: 50, direction: BUY, fee: 0 },
     ]
   }
 ];
