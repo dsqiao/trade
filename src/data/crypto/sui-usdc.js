2d50a7b26fe0dc59bb6ab56d5ef84a5a624bb9e4
dsqiao <2420392704@qq.com>
2025-04-28 11:57:03 +0800

    update

--- src/data/crypto/sui-usdc.js
+++ src/data/crypto/sui-usdc.js
@@ -27,6 +27,7 @@ const suiTradeData = [
   { date: '2025.4.25', digest: 'HYkq4wnzxvetEu64HBf7eivhjCGBcLHApSU93Bg4peY6', direction: BUY, sui: 200.173017715, usdc: 710.000027, gas: 0.022617564 },
   { date: '2025.4.27', digest: 'J1qNKBXv3UEafxGdtRSM8JHKRqwQVLq5FSYmYk7PNnUq', direction: SELL, sui: 200.000002686, usdc: 718.256889, gas: 0.026149386 },
   { date: '2025.4.28', digest: '4YBEyZjHA3ZTM96f4ZvrXHkRKh9RZrAuV2fxKeA7kwKL', direction: BUY, sui: 200.401500648, usdc: 695.000017, gas: 0.025449208 },
+  { date: '2025.4.28', digest: 'G8tGkU7fvfoGUkaM2Rs48TRovn2LpQMVsh5PgbS6ZHvH', direction: SELL, sui: 200.000002162, usdc: 712.545617, gas: 0.02326197 },
 ];
 
 export {
