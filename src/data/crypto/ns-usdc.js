f25f408535e715910e27eebcd9a18ba43e5e6f2f
dsqiao <2420392704@qq.com>
2025-05-02 15:45:04 +0800

    update

--- src/data/crypto/ns-usdc.js
+++ src/data/crypto/ns-usdc.js
@@ -1,4 +1,4 @@
-import { BUY, SELL } from "../const";
+import { SELL } from "../const";
 
 const nsTradeData = [
   { date: '2025.4.28', digest: '5K7AChtta3FwtmvUBt3ddpbfAyuLDoxzSJJBp7La2FTf', direction: SELL, ns: 2000, usdc: 490.73409, gas: 0.002257352 },
