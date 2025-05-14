deec7e283bb0426c439c342a174735fad8f2b446
dsqiao <2420392704@qq.com>
2025-05-14 14:18:24 +0800

    update

--- src/data/crypto/ns-usdc.js
+++ src/data/crypto/ns-usdc.js
@@ -4,6 +4,7 @@ const nsTradeData = [
   { date: '2025.4.28', digest: '5K7AChtta3FwtmvUBt3ddpbfAyuLDoxzSJJBp7La2FTf', direction: SELL, ns: 2000, usdc: 490.73409, gas: 0.002257352 },
   { date: '2025.5.4', digest: '2zQPGYgWq6MnJGRo9616vJhVfuMniksQX616cipwvqkB', direction: BUY, ns: 1980.672107, usdc: 400.270332, gas: 0.03943918 + 0.04588, t: '1' },
   { date: '2025.5.8', digest: 'Ahrv7m9vZt8HZ1DdJuXz6K8UL46bqCLeNVC5uaHzdbNu', direction: SELL, ns: 1980, usdc: 437.730916, gas: 0.011188104 + 0.017647348, t: '1 | + 37u' },
+  { date: '2025.5.14', digest: '43GSuSBkgJcxnjGG6qqfTQxQXvUu4kZnpkF1A57eUq9W', direction: SELL, ns: 2000, usdc: 518.700459, gas: 0.001868816 + 0.017604168 },
 ];
 
 export {
