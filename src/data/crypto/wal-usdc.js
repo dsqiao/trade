89e23ae8d1d6b0863b4c90aca1510e67c949075f
dsqiao <2420392704@qq.com>
2025-06-05 09:46:05 +0800

    update

---
+++ src/data/crypto/wal-usdc.js
@@ -0,0 +1,9 @@
+import { BUY } from "../const";
+
+const walData = [
+  { date: '2025.6.4', digest: '2fafnYvCvLJ4Jk3Mwd3wXrV89GPkQZp2VPQhU6Yai1rw', direction: BUY, usdc: 1600, wal: 3271.396772113, gas: 0.024718334 }
+];
+
+export {
+  walData
+};
