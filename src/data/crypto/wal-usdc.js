080819d2d5f1d8fab48a38501efeca9c7fbe2356
dsqiao <2420392704@qq.com>
2025-06-10 09:47:29 +0800

    update

--- src/data/crypto/wal-usdc.js
+++ src/data/crypto/wal-usdc.js
@@ -4,7 +4,9 @@ const walTradeData = [
   { date: '2025.6.4', digest: '2fafnYvCvLJ4Jk3Mwd3wXrV89GPkQZp2VPQhU6Yai1rw', direction: BUY, usdc: 1600, wal: 3271.396772113, gas: 0.024718334 },
   { date: '2025.6.5', digest: '4Vg1Nx6L5uexqwJEuzrgQCsexxbaYr67hnF9RKoZYGvK', direction: BUY, usdc: 500, wal: 1041.108858943, gas: -0.007583076 },
   { date: '2025.6.9', digest: 'EDuFUxXWMSVFfQ1BE8jRyqtK6t22hsL4XF5cdH3wgtpZ', direction: SELL, usdc: 527.292184, wal: 1040.000170777, gas: 0.004465584 }, 
-  { date: '2025.6.9', digest: 'HwZKX3435px23TeftNjnYASoVke7dWbDjvuwJDTDFv2p', direction: SELL, usdc: 513.260825, wal: 1000.000109388, gas: 0.009163586 + 0.006226796 }
+  { date: '2025.6.9', digest: 'HwZKX3435px23TeftNjnYASoVke7dWbDjvuwJDTDFv2p', direction: SELL, usdc: 513.260825, wal: 1000.000109388, gas: 0.009163586 + 0.006226796 },
+  { date: '2025.6.9', digest: 'S9ARVTgduffuCHDga1mWjNcEpYaes1Txo4Fu3uwSfAb', direction: SELL, usdc: 520.07366, wal: 1000, gas: 0.007741288 + 0.004241068 },
+  { date: '2025.6.9', digest: '5nCvKR1VzT1ciZXKwC3oG1XcPzJgunNdJ4cWw2Nm5W9q', direction: SELL, usdc: 684.457692, wal: 1304.039575128, gas: 0.006370236 + 0.006226796 },
 ];
 
 export {
