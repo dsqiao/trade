8f378dfec29df56b76925ee5cd695d314ee5e994
dsqiao <2420392704@qq.com>
2025-10-21 02:22:54 +0800

    update

--- src/data/stock/mara.js
+++ src/data/stock/mara.js
@@ -482,7 +482,8 @@ const data = [
       { day: 16, price: 20, number: 150, direction: BUY, fee: 1.46, t: 123, desc: '多委托了一单' },
       { day: 16, price: 20.4, number: 150, direction: SELL, fee: 1.48, t: 123, desc: '一发现就把多委托的赶紧卖了' },
       { day: 20, price: 21, number: 50, direction: SELL, fee: 0 },
-      { day: 20, price: 22, number: 50, direction: SELL, fee: 0 },
+      { day: 20, price: 22, number: 50, direction: SELL, fee: 0, t: 124 },
+      { day: 20, price: 21, number: 50, direction: BUY, fee: 0, t: 124 },
     ]
   }
 ];
