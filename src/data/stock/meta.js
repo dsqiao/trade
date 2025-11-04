3340623a058e64dcf474cbfb1794cef3935339cb
dsqiao <2420392704@qq.com>
2025-11-04 21:24:09 +0800

    update

--- src/data/stock/meta.js
+++ src/data/stock/meta.js
@@ -4,14 +4,18 @@ const currentPrice = 668;
 const data = [ {
   month: '202510',
   trans: [
-    { day: 29, price: 701, number: 3, direction: BUY, fee: 0 },
-    { day: 30, price: 695, number: 3, direction: BUY, fee: 0 },
-    { day: 30, price: 673, number: 3, direction: BUY, fee: 0 },
-    { day: 30, price: 660, number: 3, direction: BUY, fee: 0 },
+    { day: 29, price: 701, number: 3, direction: BUY, fee: 0.38 },
+    { day: 30, price: 695, number: 3, direction: BUY, fee: 1.02 },
+    { day: 30, price: 673, number: 3, direction: BUY, fee: 1.02 },
+    { day: 30, price: 660, number: 3, direction: BUY, fee: 1.02 },
+    { day: 31, price: 650, number: 3, direction: BUY, fee: 1.02 },
   ]
 }, {
   month: '202511',
-  trans: []
+  trans: [
+    { day: 3, price: 638, number: 3, direction: BUY, fee: 0 },
+    { day: 4, price: 630, number: 3, direction: BUY, fee: 0 },
+  ]
 } ];
 
 export { data, currentPrice };
