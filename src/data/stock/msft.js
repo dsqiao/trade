49f3222862b6e695ee87e7ff8f1292c06d32cd1b
dsqiao <2420392704@qq.com>
2026-06-21 23:11:12 +0800

    update

--- src/data/stock/msft.js
+++ src/data/stock/msft.js
@@ -1,4 +1,4 @@
-import { BUY, SELL } from "../const.js";
+import { BUY, OTHER, SELL } from "../const.js";
 
 const currentPrice = 0;
 const data = [ {
@@ -16,12 +16,20 @@ const data = [ {
     { day: 29, price: 445, number: 44, direction: BUY, fee: 1.13, },
   ]
 }, {
+  month: '202603',
+  trans: [
+    { day: 13, price: 0, number: 0, direction: OTHER, fee: -40.04, desc: '现金分红 44 股 * 0.91 USD/股' },
+    { day: 13, price: 0, number: 0, direction: OTHER, fee: 4.00, desc: '企业行动费用' },
+  ]
+}, {
   month: '202606',
   trans: [
     { day: 3, price: 436, number: 5, direction: BUY, fee: 1.03 },
     { day: 8, price: 412, number: 1, direction: BUY, fee: 0.01 },
     { day: 9, price: 402, number: 2, direction: BUY, fee: 0.02 },
     { day: 10, price: 397, number: 2, direction: BUY, fee: 0.02 },
+    { day: 12, price: 0, number: 0, direction: OTHER, fee: -40.04, desc: '现金分红 44 股 * 0.91 USD/股' },
+    { day: 12, price: 0, number: 0, direction: OTHER, fee: 4.00, desc: '企业行动费用' },
     { day: 17, price: 378, number: 3, direction: BUY, fee: 0.02 },
   ]
 } ];
