addd1b4486a42cfb5de14efa09a4b250a315ce7c
dsqiao <2420392704@qq.com>
2026-06-26 02:42:18 +0800

    update

--- src/data/stock/mara.js
+++ src/data/stock/mara.js
@@ -519,10 +519,10 @@ const data = [
       { day: 22, price: 14.9, number: 100, direction: BUY, fee: 1.31, t: 134 },
       { day: 22, price: 15.23, number: 100, direction: SELL, fee: 1.36, t: 134 },
       { day: 22, price: 14.85, number: 100, direction: BUY, fee: 1.31, t: 135 },
-      { day: 23, price: 15.40, number: 100, direction: SELL, fee: 0, t: 135 },
-      { day: 23, price: 14.5, number: 100, direction: BUY, fee: 0, t: 136 },
-      { day: 23, price: 15, number: 100, direction: SELL, fee: 0, t: 136 },
-      { day: 24, price: 13.98, number: 100, direction: BUY, fee: 0 },
+      { day: 23, price: 15.40, number: 100, direction: SELL, fee: 1.36, t: 135 },
+      { day: 23, price: 14.5, number: 100, direction: BUY, fee: 1.31, t: 136 },
+      { day: 23, price: 15, number: 100, direction: SELL, fee: 1.36, t: 136 },
+      { day: 24, price: 13.98, number: 100, direction: BUY, fee: 1.31 },
       { day: 25, price: 13.42, number: 92, direction: BUY, fee: 0 },
     ]
   }
