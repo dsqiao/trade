91e43f33fc96773f9ab4e6aa6bd85722ed8e0772
dsqiao <2420392704@qq.com>
2026-07-01 01:56:20 +0800

    feat: NVDA 分红

--- src/data/stock/nvda.js
+++ src/data/stock/nvda.js
@@ -17,6 +17,8 @@ const data = [ {
     { day: 6, price: 458, number: 5, direction: BUY, fee: 1.02 },
     { day: 18, price: 499, number: 5, direction: SELL, fee: 1.05 },
     { day: 18, price: 501.1, number: 5, direction: SELL, fee: 1.05 },
+    { day: 29, price: 0, number: 0, direction: OTHER, fee: -0.68, desc: '0.04/股 * 17 股' },
+    { day: 29, price: 0, number: 0, direction: OTHER, fee: 0.07, desc: 'TAX' },
   ]
 }, {
   month: '202401',
@@ -25,8 +27,6 @@ const data = [ {
     { day: 8, price: 520, number: 5, direction: SELL, fee: 1.05 },
     { day: 9, price: 533.35, number: 4, direction: SELL, fee: 1.04 },
     { day: 9, price: 542.5, number: 1, direction: SELL, fee: 1.02 },
-    // { day: 8, price: 0, number: 0, direction: OTHER, fee: -14.18, desc: '现金分红 27 股 * 0.525 USD/股' },
-    // { day: 25, price: 0, number: 0, direction: OTHER, fee: 1.42, desc: 'Tax/Dividend Fee' },
   ]
 }, {
   month: '202508',
@@ -42,6 +42,8 @@ const data = [ {
 }, {
   month: '202510',
   trans: [
+    { day: 3, price: 0, number: 0, direction: OTHER, fee: -0.20, desc: '0.01/股  * 20 股' },
+    { day: 3, price: 0, number: 0, direction: OTHER, fee: 0.02, desc: 'Tax/Dividend fee' },
     { day: 10, price: 190, number: 5, direction: BUY, fee: 1.03 },
     { day: 10, price: 186, number: 5, direction: BUY, fee: 1.03 },
     { day: 10, price: 181, number: 5, direction: BUY, fee: 1.03 },
@@ -53,6 +55,24 @@ const data = [ {
   trans: [
     { day: 6, price: 186.83, number: 5, direction: BUY, fee: 1.03 },
     { day: 7, price: 183, number: 5, direction: BUY, fee: 1.03 },
+  ]
+}, {
+  month: '202512',
+  trans: [
+    { day: 29, price: 0, number: 0, direction: OTHER, fee: -0.6, desc: '0.01/股 * 60 股分红' },
+    { day: 29, price: 0, number: 0, direction: OTHER, fee: 0.06, desc: 'Tax/Dividend fee' },
+  ]
+}, {
+  month: '202604',
+  trans: [
+    { day: 2, price: 0, number: 0, direction: OTHER, fee: -0.6, desc: '0.01/股 * 60 股分红' },
+    { day: 2, price: 0, number: 0, direction: OTHER, fee: 0.06, desc: 'Tax/Dividend fee' },
+  ]
+}, {
+  month: '202606',
+  trans: [
+    { day: 29, price: 0, number: 0, direction: OTHER, fee: -15, desc: '0.25 / 股 * 60 股' },
+    { day: 29, price: 0, number: 0, direction: OTHER, fee: 1.49, desc: 'Tax/Dividend fee' },
   ]
 } ];
 
