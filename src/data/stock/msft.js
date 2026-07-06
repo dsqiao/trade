1c255d188563aa14177a509390e08342654e1d0b
dsqiao <2420392704@qq.com>
2026-07-06 19:52:44 +0800

    update

--- src/data/stock/msft.js
+++ src/data/stock/msft.js
@@ -39,7 +39,8 @@ const data = [ {
 }, {
   month: '202607',
   trans: [
-    { day: 2, price: 392, number: 5, direction: SELL, fee: 0 },
+    { day: 2, price: 392, number: 5, direction: SELL, fee: 0, t: 4 },
+    { day: 6, price: 389, number: 5, direction: BUY, fee: 0, t: 4 },
   ]
 } ];
 
