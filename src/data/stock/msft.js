95d2e6e706020778a2eda8127bb0e9a911c00a7c
dsqiao <2420392704@qq.com>
2026-08-18 10:20:01 +0800

    feat: update

--- src/data/stock/msft.js
+++ src/data/stock/msft.js
@@ -54,6 +54,9 @@ const data = [ {
     { day: 3, price: 485, number: 5, direction: SELL, fee: 1.09 },
     { day: 4, price: 495, number: 5, direction: SELL, fee: 1.09 },
     { day: 7, price: 505, number: 5, direction: SELL, fee: 1.09 },
+    { day: 17, price: 482.955, number: 5, direction: BUY, fee: 0, t: 6 },
+    { day: 17, price: 485.12, number: 5, direction: SELL, fee: 0, t: 6 },
+    { day: 17, price: 481.49, number: 5, direction: BUY, fee: 0 },
   ]
 } ];
 
