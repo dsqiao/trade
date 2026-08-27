1124af66ccca34af8f4a6ccaa2e0f3893a7a551b
dsqiao <2420392704@qq.com>
2026-08-27 10:36:52 +0800

    update

--- src/data/stock/nvda.js
+++ src/data/stock/nvda.js
@@ -87,7 +87,8 @@ const data = [ {
     { day: 5, price: 222, number: 5, direction: SELL, fee: 1.06 },
     { day: 24, price: 210, number: 5, direction: BUY, fee: 1.03, t: 3 },
     { day: 25, price: 213.5, number: 5, direction: SELL, fee: 1.06, t: 3 },
-    { day: 26, price: 210, number: 5, direction: BUY, fee: 0, }, // fee todo
+    { day: 26, price: 210, number: 5, direction: BUY, fee: 0, t: 4 }, // fee todo
+    { day: 26, price: 220, number: 5, direction: SELL, fee: 0, t: 4 }, // fee todo
   ]
 } ];
 
